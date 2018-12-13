d1dce12f10dc409d80fad86db041b32ea2adccca
Tarciso Loiola <tarcisoloiola@gmail.com>
2018-12-12 20:58:31 -0500

    caledar works

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -15,11 +15,11 @@ class Calendar extends Component {
     }
 
     handleMonth(e){
-        let thisMonth = Moment()
-        let stateMonth = this.state.month
+        let currentMonth = Moment()
+        let thisStateMonth = this.state.month
 
         let newState = this.state.month
-        newState = e < 0 && thisMonth < stateMonth ? Moment(newState).subtract(1, 'months') : e > 0 ? Moment(newState).add(1, 'months') : Moment(newState)
+        newState = e < 0 && currentMonth < thisStateMonth ? Moment(newState).subtract(1, 'months') : e > 0 ? Moment(newState).add(1, 'months') : Moment(newState)
         this.setState({month: newState})
     }
 
@@ -28,39 +28,28 @@ class Calendar extends Component {
 
         const arrowLeft = '<'
         const arrowRight = '>'
-
-        // let currentTime = Moment().format('h:mm a')
+        
         let today = Moment().format('DD')
-        // let thisYear = Moment().format('YYYY')
-        // let thisStateYear = this.state.month.format('YYYY')
-        
         let daysInMonth =  this.state.month.daysInMonth()
-        // let monthYearDate = this.state.month.format('MMMM, YYYY')
-        
-        let month = this.state.month.format('MM')
-        console.log(month)
 
-        let _1ofMonth_WeekDay = Moment().month(month-1).date(1).format('dddd')
+        let currentWeekDay = Moment().format('ddd').toLowerCase()
         
-        let emptyWeekDay = 0
-        if(_1ofMonth_WeekDay.toLowerCase() === 'monday') emptyWeekDay = 1
-        if(_1ofMonth_WeekDay.toLowerCase() === 'tuesday') emptyWeekDay = 2
-        if(_1ofMonth_WeekDay.toLowerCase() === 'wednesday') emptyWeekDay = 3
-        if(_1ofMonth_WeekDay.toLowerCase() === 'thursday') emptyWeekDay = 4
-        if(_1ofMonth_WeekDay.toLowerCase() === 'friday') emptyWeekDay = 5
-        if(_1ofMonth_WeekDay.toLowerCase() === 'saturday') emptyWeekDay = 6
         
-        // console.log(emptyWeekDay)
+
+
+
+
+
 
         for( let i = 1; i <= daysInMonth; i +=1 ) {
-            if(emptyWeekDay > 0 ){
-                for (let j = 0; j < emptyWeekDay; j += 1) {
-                    dayArray.push(<div key={`emptyWeekDay${j}`}></div>)
-                }
-                emptyWeekDay = 0
-            }
-            if(i < today) dayArray.push(<div key={`dayArray${i}`} className='weekdays weekdays-disable'>{i}</div>)
-            else dayArray.push(<div key={`dayArray${i}`} className='weekdays' onClick={e => this.handleDatePicker(i)}>{i}</div>)
+            // if(emptyWeekDay > 0 ){
+            //     for (let j = 0; j < emptyWeekDay; j += 1) {
+            //         dayArray.push(<div key={`emptyWeekDay${j}`}></div>)
+            //     }
+            //     emptyWeekDay = 0
+            // }
+            // if(i < today) dayArray.push(<div key={`dayArray${i}`} className='weekdays weekdays-disable'>{i}</div>)
+            // else dayArray.push(<div key={`dayArray${i}`} className='weekdays' onClick={e => this.handleDatePicker(i)}>{i}</div>)
         }
 
 
